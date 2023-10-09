08c56a8f7523b1ef818572e9e6f9b56b55e7c999
DESKTOP-8FK18PG\Carlos <csi23-charnte@colegioaltair.net>
2023-10-09 14:36:47 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio1Operadores;
+
+/// <summary>
+/// Aquí irá la suma de los números, aparte de darles valor.
+/// </summary>
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        int seleccion1, seleccion2, resultado; 
+       
+        
+
+        Console.WriteLine("Elige un número: ");
+        seleccion1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Escoge otro número: ");
+        seleccion2 = int.Parse(Console.ReadLine());
+        resultado = seleccion1 + seleccion2;
+        Console.WriteLine("El resultado es: " +  resultado);
+        Console.ReadLine();
+
+    }
+}
